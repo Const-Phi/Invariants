78fdb5d6fe938e5b9feaebf4551d420d465231ec
Constantine Philipchenko <const-phi@ya.ru>
2017-10-31 08:50:02 +0300

    Some examples of conditional statement use case.

--- ConditionStatementDemo/Program.cs
+++ ConditionStatementDemo/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConditionStatementDemo
+namespace ConditionalDemo
 {
     class Program
     {
         static void Main()
         {
+            #region Simple
+
             bool condition = true;
             if (condition)
             {
@@ -20,6 +18,49 @@ namespace ConditionStatementDemo
             {
                 Console.WriteLine("Condition is false");
             }
+
+            #endregion
+
+            #region Middle
+
+            bool condition1 = true, 
+                 condition2 = false;
+            if (condition1)
+                if (condition2)
+                    Console.WriteLine("Do smth");
+                else
+                    Console.WriteLine("Do smth else");
+
+            #endregion
+
+            #region Bad practice
+
+            if (condition)
+                if (condition1)
+                    if (condition2)
+                        Console.WriteLine("Very rare event!");
+
+            #endregion
+
+            #region Good practice
+
+            if (condition && condition1 && condition2)
+                Console.WriteLine("Very rare event!");
+
+            #endregion
+
+            #region Ternary statement vs conditional statement
+
+            int baseValue = 11;
+            int derivedValue = (baseValue > 0) ? baseValue * 2 : -baseValue;
+
+            int anotherValue;
+            if (baseValue > 0)
+                anotherValue = baseValue * 2;
+            else
+                anotherValue = -baseValue;
+
+            #endregion
         }
     }
 }
